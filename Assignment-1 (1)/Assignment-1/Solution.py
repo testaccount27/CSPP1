2f523ebfbb80b660a5a008aba2202ed45e6ac29b
SukeshP1995 <sukeshpabolu@gmail.com>
2018-07-25 16:18:28 +0530

    my first commit

---
+++ Assignment-1 (1)/Assignment-1/Solution.py
@@ -0,0 +1,24 @@
+# Exercise: Assignment-1
+# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+
+
+def factorial(n):
+	'''
+	n is positive Integer
+
+	returns: a positive integer, the factorial of n.
+	'''
+	# Your code here
+	
+
+
+def main():
+	data=input()
+	for i in range(int(data)):
+		a = input()
+		print(factorial(int(a)))	
+
+if __name__== "__main__":
+	main()
