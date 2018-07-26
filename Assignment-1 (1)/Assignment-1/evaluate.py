870fc78c38dab7a73a3825bc0871c042838487cb
SukeshP1995 <sukeshpabolu@gmail.com>
2018-07-26 09:44:00 +0530

    Assignment-1number of testcases passed: 1/1 and pylint score: 4/10

--- Assignment-1 (1)/Assignment-1/evaluate.py
+++ Assignment-1 (1)/Assignment-1/evaluate.py
@@ -19,9 +19,10 @@ score, totalscore = runProcess("pylint Solution.py","Your code has been rated at
 f = open('myfile', 'w')
 f.write("number of testcases passed: " + str(cases) + "/" + str(totalcases) + "\npylint score: " + str(score) + "/" + str(totalscore))  # python will convert \n to os.linesep
 f.close() 
-
+path = os.getcwd().split('\\')
+folder = path[-1]
 runProcess("git status")
 runProcess("git add .")
-runProcess("git commit -m \"number of testcases passed: " + str(cases) + "/" + str(totalcases) + " and pylint score: " + str(score) + "/" + str(totalscore) + " \"")
+runProcess("git commit -m \""+ folder +"number of testcases passed: " + str(cases) + "/" + str(totalcases) + " and pylint score: " + str(score) + "/" + str(totalscore) + " \"")
 if cases == totalcases:
 	runProcess("git push -u origin master")
